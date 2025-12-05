8e445d041de8fc9705fe44a8edf4401a178b933a
Luisfc68 <luisfc68@gmail.com>
2025-12-05 23:52:04 +0100

    add localsearch

--- Heuristics/ValidateConfig.py
+++ Heuristics/ValidateConfig.py
@@ -113,7 +113,7 @@ class ValidateConfig(object):
 
             # Validate neighborhoodStrategy
             neighborhoodStrategy = data.neighborhoodStrategy
-            if neighborhoodStrategy not in ['TaskExchange', 'Reassignment']:
+            if neighborhoodStrategy not in ['ModelExchange']:
                 raise AMMMException('neighborhoodStrategy(%s) has to be one of [Reassignment, Exchange].' % str(neighborhoodStrategy))
 
             # Validate policy
