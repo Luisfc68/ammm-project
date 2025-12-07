c5acf8b84098f3dbd2dcb191e51dce11a05b1f1f
Ivan <ivan.reolid9@gmail.com>
2025-12-07 23:04:53 +0100

    Camera manufacturers on Instance Generator

--- InstanceGeneratorProject/InstanceGenerator.py
+++ InstanceGeneratorProject/InstanceGenerator.py
@@ -1,12 +1,13 @@
-import os, random
+import os, random, math
 from AMMMGlobals import AMMMException
 
 
 class InstanceGenerator(object):
     # Generate instances based on read configuration.
 
-    def __init__(self, config):
+    def __init__(self, config, conf):
         self.config = config
+        self.conf   = conf
 
     def generate(self):
         instancesDirectory = self.config.instancesDirectory
@@ -15,23 +16,20 @@ class InstanceGenerator(object):
         numInstances = self.config.numInstances
 
         numCameras = self.config.numCameras
-        minPrice = self.config.minPrice
-        maxPrice = self.config.maxPrice
-        minRange = self.config.minRange
-        maxRange = self.config.maxRange
-        minAutonomy = self.config.minAutonomy
-        maxAutonomy = self.config.maxAutonomy
-        minPowerConsumption = self.config.minPowerConsumption
-        maxPowerConsumption = self.config.maxPowerConsumption
-
         numCrossings = self.config.numCrossings
-        minRangeRequirement = self.config.minRangeRequirement
-        maxRangeRequirement = self.config.maxRangeRequirement
+
+        map_cfg = self.conf["map_settings"]
+        mkt_cfg = self.conf["market_settings"]
+        profiles = self.conf["profiles"]
+        profile_names = list(profiles.keys())
+
+        grid_side = int(math.sqrt(numCrossings * map_cfg["density_factor"]))
 
         if not os.path.isdir(instancesDirectory):
             raise AMMMException('Directory(%s) does not exist' % instancesDirectory)
 
         for i in range(numInstances):
+            coords = [(random.uniform(0, grid_side), random.uniform(0, grid_side)) for _ in range(numCrossings)]
             instancePath = os.path.join(instancesDirectory, '%s_%d.%s' % (fileNamePrefix, i, fileNameExtension))
             fInstance = open(instancePath, 'w')
 
@@ -39,22 +37,39 @@ class InstanceGenerator(object):
             ranges = [0] * numCameras
             autonomies = [0] * numCameras
             powerConsumptions = [0] * numCameras
-            for c in range(numCameras):
-                prices[c] = random.randint(minPrice, maxPrice)
-                ranges[c] = random.randint(minRange, maxRange)
-                autonomies[c] = random.randint(minAutonomy, maxAutonomy)
-                powerConsumptions[c] = random.randint(minPowerConsumption, maxPowerConsumption)
 
+            for c in range(numCameras):
+                manufacturer = random.choices(
+                    profile_names,
+                    weights=mkt_cfg["share_weights"]
+                )[0]
+
+                specs = profiles[manufacturer]
+                ranges[c] = random.randint(*specs["range"])
+                autonomies[c] = random.randint(*specs["autonomy"])
+                powerConsumptions[c] = int(ranges[c] * random.uniform(*specs["consumption_factor"]))
+
+                prices[c] = specs["base_price"] + \
+                        (ranges[c] * specs["mult_range"]) + \
+                        (autonomies[c] * specs["mult_autonomy"])
+
+                noise = random.uniform(*mkt_cfg["price_noise"])
+                prices[c] = int(prices[c] * noise)
+                prices[c] = max(mkt_cfg["price_floor"], prices[c])
 
             rangeRequirements = [[0] * numCrossings for _ in range(numCrossings)]
+            cutoff = map_cfg["visibility_cutoff"]
+
             for i in range(numCrossings):
-                for j in range(i, numCrossings):
-                    if i == j:
-                        rangeRequirements[i][j] = 0
-                    else:
-                        value = random.randint(minRangeRequirement, maxRangeRequirement)
-                        rangeRequirements[i][j] = value
-                        rangeRequirements[j][i] = value
+                for j in range(numCrossings):
+                    if i == j: continue
+
+                    xi, yi = coords[i]
+                    xj, yj = coords[j]
+                    dist = math.sqrt((xi - xj)**2 + (yi - yj)**2)
+                    val = int(round(dist))
+
+                    rangeRequirements[i][j] = cutoff if val > cutoff else val
 
             fInstance.write('K=%d;\n' % numCameras)
             fInstance.write('N=%d;\n' % numCrossings)
@@ -66,8 +81,8 @@ class InstanceGenerator(object):
             fInstance.write('C=[%s];\n' % (' '.join(map(str, powerConsumptions))))
 
             fInstance.write('M=[\n')
-            for i in range(numCrossings):
-                fInstance.write('\t[%s]\n' % (' '.join(map(str, rangeRequirements[i]))))
+            for i in rangeRequirements:
+                fInstance.write("  " + str(i).replace(',', '') + ",\n")
             fInstance.write('];')
 
             fInstance.close()
--- InstanceGeneratorProject/Main.py
+++ InstanceGeneratorProject/Main.py
@@ -1,19 +1,28 @@
 import sys
+import os
+import json
 from Heuristics.datParser import DATParser
 from AMMMGlobals import AMMMException
 from InstanceGeneratorProject.ValidateConfig import ValidateConfig
 from InstanceGeneratorProject.InstanceGenerator import InstanceGenerator
 
+def load_config(config_file="config.json"):
+    if not os.path.exists(config_file):
+        raise FileNotFoundError(f"Configuration file not found: {config_file}")
+    with open(config_file, 'r') as f:
+        return json.load(f)
+
 def run():
     try:
         configFile = "config/config.dat"
+        confNew = load_config("config/config.json")
         print("AMMM Instance Generator")
         print("-----------------------")
         print("Reading Config file %s..." % configFile)
         config = DATParser.parse(configFile)
         ValidateConfig.validate(config)
         print("Creating Instances...")
-        instGen = InstanceGenerator(config)
+        instGen = InstanceGenerator(config, confNew)
         instGen.generate()
         print("Done")
         return 0
